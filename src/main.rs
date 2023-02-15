6d07e91484fa1f5d4604483a50115e776418aedd
sifnoc <sifnoc09@gmail.com>
2023-02-15 11:23:57 +0000

    feat: add constant for input as round constant

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod field;
+mod constants;
+
+use field::Field;
+use constants::ARK;
 
 fn main() {
-    println!("Hello, world!");
+ let f128 = Field::new(340282366920938463463374557953744961537, 23953097886125630542083529559205016746);
+ let rand = f128.rand();
+ println!("random generated from f128 Field: {}", rand);
+ println!("first round constant: {}", ARK[0]);
 }

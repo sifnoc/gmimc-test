d95171cfb855aa7660ed6b3978288bd0d4d5f9dc
sifnoc <sifnoc09@gmail.com>
2023-02-21 08:49:04 +0000

    chore: make up code and clean up

--- benches/gmimc_perf.rs
+++ benches/gmimc_perf.rs
@@ -9,7 +9,12 @@ pub const G: u128 = 23953097886125630542083529559205016746;
 
 fn hash_performance(bench: &mut Bencher) {
   let f128 = field::Field::new(M, G);
-  // let f128_2 = field::Field::new(M, G);
+
+  let v1 = f128.rand();
+  let v2 = f128.rand();
+  let v3 = f128.rand();
+  let v4 = f128.rand();
+  
   let g = gmimc::GMiMC_erf {
     field: f128,
     capacity: 5,
@@ -17,11 +22,6 @@ fn hash_performance(bench: &mut Bencher) {
     round: 166,
   };
 
-  let v1 = g.field.rand();
-  let v2 = g.field.rand();
-  let v3 = g.field.rand();
-  let v4 = g.field.rand();
-  
   bench.iter(|| {
     g.get_hash_output(&[v1, v2, v3, v4]);
   })

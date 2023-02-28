e91b435073df0b6951ad203b3a9cbe79af87748a
sifnoc <sifnoc09@gmail.com>
2023-03-01 07:51:03 +0900

    chore: remove un-necessaries

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-// mod constants;
-// mod gmimc;
-
-use std::fmt::Write;
-use std::{mem, slice};
-
 use gmimc_rust_test::constants::ARK;
 use gmimc_rust_test::field::Field;
-
 
 fn main() {
  let f128 = Field::new(340282366920938463463374557953744961537, 23953097886125630542083529559205016746);

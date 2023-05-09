280e4ff8e4800101bbc81d55a12970ccbb9729b6
sifnoc <sifnoc09@gmail.com>
2023-05-09 17:36:51 +0900

    fix: bench scripts with new ff

--- benches/gmimc_perf.rs
+++ benches/gmimc_perf.rs
@@ -1,32 +1,34 @@
 #[macro_use]
 extern crate bencher;
+use gmimc_rust_test::gmimc::gmimc_erf;
 
 use bencher::Bencher;
-use gmimc_rust_test::{field, gmimc};
-
-pub const M: u128 = 340282366920938463463374557953744961537;
-pub const G: u128 = 23953097886125630542083529559205016746;
+use ff::PrimeField;
+use rand::prelude::random;
 
 fn hash_performance(bench: &mut Bencher) {
-  let f128 = field::Field::new(M, G);
+    #[derive(PrimeField)]
+    #[PrimeFieldModulus = "340282366920938463463374557953744961537"]
+    #[PrimeFieldGenerator = "23953097886125630542083529559205016746"]
+    #[PrimeFieldReprEndianness = "little"]
+    struct F([u64; 3]);
 
-  let v1 = f128.rand();
-  let v2 = f128.rand();
-  let v3 = f128.rand();
-  let v4 = f128.rand();
-  
-  let g = gmimc::gmimc_erf {
-    field: f128,
-    capacity: 5,
-    words: 4,
-    round: 166,
-  };
+    let gmimc = gmimc_erf::<F, 6> {
+        capacity: 5,
+        words: 4,
+        round: 166,
+        _field: std::marker::PhantomData::<F>,
+    };
 
-  bench.iter(|| {
-    g.get_hash_output(&[v1, v2, v3, v4]);
-  })
+    bench.iter(|| {
+        gmimc.get_hash_output(&[
+            random::<u128>(),
+            random::<u128>(),
+            random::<u128>(),
+            random::<u128>(),
+        ]);
+    })
 }
-
 
 benchmark_group!(benches, hash_performance);
 benchmark_main!(benches);
